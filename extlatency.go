7a7d97c046c655e7feb2213c5bb67743899131ed
Ricardo Carrasco <ricardo@carrasco.dev>
2025-05-20 16:44:43 +0000

    inish linear parsing

--- extlatency.go
+++ extlatency.go
@@ -1,11 +1,29 @@
 package extlatency
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
+
+func getDescriptionMap() map[string]string {
+	filePath := "descriptions.json"
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	var data map[string]string
+	err = json.Unmarshal(fileContent, &data)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+	// fmt.Println("Parsed JSON data:", data)
+	return data
+}
 
 func Parse(log string) any {
 	datapowerLogRegex := regexp.MustCompile(`(?:ExtLatency: )(.*)(?: == )(.*)(\[.*\])$`) // must try first
@@ -17,48 +35,58 @@ func Parse(log string) any {
 		fmt.Println("Handle as APIC Log")
 		match := apiGatewayLogRegex.FindStringSubmatch(log)
 		if len(match) > 0 {
-			// fmt.Println(match[0])
-			actionsRaw := strings.Split(match[1], ",")
-			// fmt.Println(actionsRaw)
-
-			parseActions(actionsRaw)
-			// logUrl := match[2]
-			// logUrl = logUrl[1: len(logUrl)-1]
-			// fmt.Println(logUrl)
-
+			actionsRaw := strings.Trim(match[1], ", ")
+			actionsRawSplit := strings.Split(actionsRaw, ",")
+			rawActions := parseActionsBase(actionsRawSplit)
+			// fmt.Println(rawActions)
+			actions := parseActions(rawActions)
+			fmt.Println(actions)
 		}
 	} else {
 		fmt.Println("Handle as APIC Log")
 	}
-
 	return log
 }
 
-type Action struct {
+type BaseAction struct {
 	keyword string
-	start   int64
-	// duration    int
-	description string
+	elapsed int
+}
+type Action struct {
+	BaseAction
+    description string
+	duration    int
 }
 
-func NewAction(keyword string, start int64) Action {
-	return Action{
-		keyword:     keyword,
-		start:       start,
-		description: "",
-	}
-}
-func parseActions(actionsRaw []string) {
-	var actionsParsed []Action
-	for _, actionRaw := range actionsRaw {
-		if strings.TrimSpace(actionRaw) != "" {
-			// fmt.Println("Action" + actionRaw)
-			splitStrs := strings.Split(actionRaw, "=")
-			keyword := splitStrs[0]
-			start, _ := strconv.ParseInt(splitStrs[1], 0, 64)
-			actionsParsed = append(actionsParsed, NewAction(keyword, start))
+func parseActions(baseActions []BaseAction) []Action {
+	var actions []Action
+	descMap := getDescriptionMap()
+	for i, baseAction := range baseActions {
+		duration := 0
+		if i != 0 {
+			duration = baseAction.elapsed - baseActions[i-1].elapsed
 		}
+		action := Action{
+            BaseAction: baseAction,
+			description: descMap[baseAction.keyword],
+			duration:    duration,
+		}
+		actions = append(actions, action)
 	}
-	fmt.Println(actionsParsed)
+	return actions
+}
 
+func parseActionsBase(actionsRawSplit []string) []BaseAction {
+	var actions []BaseAction
+	for _, actionStrRaw := range actionsRawSplit {
+		splitStrs := strings.Split(actionStrRaw, "=")
+		keyword := splitStrs[0]
+		elapsed, err := strconv.ParseInt(splitStrs[1], 0, 64)
+		if err != nil {
+			log.Fatalf("Failed to parse action %s elapsed time", keyword)
+		}
+		action := BaseAction{keyword, int(elapsed)}
+		actions = append(actions, action)
+	}
+	return actions
 }
